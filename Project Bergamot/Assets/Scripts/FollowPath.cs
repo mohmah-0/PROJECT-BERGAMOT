4155f2f7bc1f58a104955560e1845b9567e25b58
Allkams <Allkams02@gmail.com>
2022-02-02 14:37:23 +0100

    Small Fixes with CameraMovement

--- Project Bergamot/Assets/Scripts/FollowPath.cs
+++ Project Bergamot/Assets/Scripts/FollowPath.cs
@@ -10,6 +10,7 @@ public class FollowPath : MonoBehaviour
     public GameObject speedTargetObject; //Will be used when car mechanics is done!
     public float speed = 5;
     public Vector3 offset;
+    public float smoothTurn = 0.125f;
     float distanceTravelled;
 
     float vertical;
@@ -25,7 +26,10 @@ public class FollowPath : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
 
         distanceTravelled += vertical * speed * Time.deltaTime;
-        transform.position = pathToFollow.path.GetPointAtDistance(distanceTravelled) + offset;
-        transform.rotation = pathToFollow.path.GetRotationAtDistance(distanceTravelled) * Quaternion.Euler(45, 0, 0);
+        Vector3 targetpos = pathToFollow.path.GetPointAtDistance(distanceTravelled) + offset;
+        Quaternion targetrot = pathToFollow.path.GetRotationAtDistance(distanceTravelled) * Quaternion.Euler(45, 0, 0);
+
+        transform.position = targetpos;
+        transform.rotation = Quaternion.Lerp(transform.rotation ,targetrot, smoothTurn);
     }
 }
