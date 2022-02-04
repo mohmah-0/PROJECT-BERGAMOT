057ae92b0d1b41c37f73629ea84e96d8e7efefc6
Allkams <Allkams02@gmail.com>
2022-02-04 14:41:41 +0100

    Some minor changes to the follow cam
    Now following the car, in a bad way but works for now. Will fix on monday

--- Project Bergamot/Assets/Scripts/FollowPath.cs
+++ Project Bergamot/Assets/Scripts/FollowPath.cs
@@ -7,29 +7,28 @@ public class FollowPath : MonoBehaviour
 {
 
     public PathCreator pathToFollow;
-    public GameObject speedTargetObject; //Will be used when car mechanics is done!
-    public float speed = 5;
+    public Rigidbody speedTargetObject; //Will be used when car mechanics is done!
     public Vector3 offset;
     public float smoothTurn = 0.125f;
     float distanceTravelled;
 
-    float vertical;
 
     void Start()
     {
-        transform.position = pathToFollow.path.GetPointAtDistance(0);
+        transform.position = pathToFollow.path.GetPointAtDistance(speedTargetObject.position.x);
+        distanceTravelled = speedTargetObject.position.x-25;
     }
 
     // Update is called once per frame
     void Update()
     {
-        vertical = Input.GetAxis("Vertical");
 
-        distanceTravelled += vertical * speed * Time.deltaTime;
+
+        distanceTravelled += speedTargetObject.velocity.magnitude * Time.deltaTime;
         Vector3 targetpos = pathToFollow.path.GetPointAtDistance(distanceTravelled) + offset;
         Quaternion targetrot = pathToFollow.path.GetRotationAtDistance(distanceTravelled) * Quaternion.Euler(45, 0, 0);
 
         transform.position = targetpos;
-        transform.rotation = Quaternion.Lerp(transform.rotation ,targetrot, smoothTurn);
+        transform.rotation = Quaternion.Slerp(targetrot, speedTargetObject.rotation, smoothTurn);
     }
 }
