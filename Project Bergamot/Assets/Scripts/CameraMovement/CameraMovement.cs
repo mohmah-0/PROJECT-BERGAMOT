159bc0ceb72ba0f6b9a4a5b1feb8dc59f40a3f0e
Allkams <Allkams02@gmail.com>
2022-02-07 15:00:04 +0100

    Small Fixes

--- Project Bergamot/Assets/Scripts/CameraMovement/CameraMovement.cs
+++ Project Bergamot/Assets/Scripts/CameraMovement/CameraMovement.cs
@@ -3,38 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using PathCreation;
 
-public class FollowCar : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
     public Rigidbody leadcar;
     public PathCreator FollowPath;
     public Vector3 offset;
-    public float damping = 1f;
+    float damping = 1f;
 
-    public float distance;
-    public Vector3 path;
-
-    float speed;
-
-    //Vector3 path;
-
-    void Start()
-    {
-        distance = (leadcar.transform.position - FollowPath.path.GetPointAtDistance(0)).magnitude;
-    }
+    Vector3 pathPoint;
 
     // Update is called once per frame
     void Update()
     {
-        speed = leadcar.velocity.magnitude;
-        distance += speed * Time.deltaTime;
+
         float currentAngle = transform.eulerAngles.y;
         float desiredAngle = leadcar.transform.eulerAngles.y;
         float angle = Mathf.LerpAngle(currentAngle, desiredAngle, damping);
 
-        path = FollowPath.path.GetClosestPointOnPath(leadcar.position);
+        pathPoint = FollowPath.path.GetClosestPointOnPath(leadcar.position);
         
         Quaternion rotation = Quaternion.Slerp(FollowPath.path.GetRotationAtDistance(FollowPath.path.GetClosestDistanceAlongPath(leadcar.position)),Quaternion.Euler(0f, angle, 0f),0.125f);
-        transform.position = path - (rotation * offset);
+        transform.position = pathPoint - (rotation * offset);
 
         transform.LookAt(leadcar.transform.position);
 
