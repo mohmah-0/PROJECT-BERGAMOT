2564246c5c092f37140c683543611e0e5100432b
Edwin Mak <edwinmak00@hotmail.com>
2022-02-08 16:02:20 +0100

    ui update

--- Project Bergamot/Assets/Scripts/UI/carDisplay.cs
+++ Project Bergamot/Assets/Scripts/UI/carDisplay.cs
@@ -5,32 +5,26 @@ using UnityEngine.UI;
 
 public class carDisplay : MonoBehaviour
 {
-    //public createCar Car;
     public List<createCar> carList = new List<createCar>();
     public GameObject carCellPrefab;
 
-    [Header("Properties") ]
-    [SerializeField] private Text nameText;
-    [SerializeField] private Image artworkImage;
+
+     private Text nameText;
+     private Image artworkImage;
 
     private void Start()
     {
-        // Display all the car in the list 
-        //loop through
         foreach(createCar Car in carList)
         {
             spawnCarCellUI(Car);
-
         }
-       // nameText.text = Car.carText;
-       // artworkImage.sprite = Car.carImage;
     }
 
     //function to display them
     private void spawnCarCellUI(createCar car)
     {
         GameObject carCell = Instantiate(carCellPrefab,transform);
-
+        Debug.Log(carCell, carCell );   
         artworkImage = carCell.transform.Find("Model").GetComponent<Image>();
         nameText = carCell.transform.Find("Model_text").GetComponent<Text>();
 
---
+++ Project Bergamot/Assets/Scripts/UI/playerSelection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerSelection : MonoBehaviour
+{
+    public GameObject playerCellPrefab;
+    public GameObject carCellPrefab;
+
+    public void showSelectedInSlot()
+    {
+        Debug.Log(playerCellPrefab, this);
+        Image carCellImage = carCellPrefab.transform.Find("Model").GetComponent<Image>();
+        Image selectedCarImage = playerCellPrefab.transform.Find("Image").GetComponent<Image>();
+        selectedCarImage.sprite = carCellImage.sprite;
+        Debug.Log("selected image: " + selectedCarImage.sprite);
+    }
+}
