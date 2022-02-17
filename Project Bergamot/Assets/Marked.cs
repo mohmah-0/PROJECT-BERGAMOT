20c5d0d93c88ee63328dfb39afbe3e1590749e47
mohmah-0 <93762870+mohmah-0@users.noreply.github.com>
2022-02-17 14:45:22 +0100

    håller på

---
+++ Project Bergamot/Assets/Marked.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marked : MonoBehaviour
+{
+    public static List<GameObject> playerObject = new List<GameObject>();
+
+    public List<string> playerName2 = new List<string>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
