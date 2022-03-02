32ce517110617a383882730e7e98f9e76909de12
Edwin Mak <edwinmak00@hotmail.com>
2022-03-02 14:25:23 +0100

    played with the sound for the vfx

---
+++ Project Bergamot/Assets/Prefabs/VFX/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    [SerializeField] AudioSource soundFile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundFile = GetComponent<AudioSource>();
+        soundFile.PlayDelayed(5.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
